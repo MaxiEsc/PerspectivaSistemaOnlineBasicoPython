61a3508c9f08135768bf110e61344bc6c2494b2e
MaxiEsc <maxescobarubp@gmail.com>
2024-09-24 17:43:56 -0300

    Idea basica de una tienda online

---
+++ online_Shop.py
@@ -0,0 +1,29 @@
+#Sistema de centass online
+
+producto = "zapatos"
+precio = 48.50
+cantidad_inv = 15
+disponibilidad = True
+
+print("** Sistema de tienda online ***")
+print("Producto: ", producto)
+print("Precio: ", precio)
+print("Cantidad de inventario: ", cantidad_inv)
+print("Disponibilidad:", disponibilidad)
+print("------------------------------------------")
+producto = "Pan"
+precio = 3.5
+cantidad_inv = 450
+disponibilidad = True
+
+print("** Sistema de tienda online ***")
+print("Producto: ", producto)
+print("Precio: ", precio)
+print("Cantidad de inventario: ", cantidad_inv)
+print("Disponibilidad:", disponibilidad)
+print("------------------------------------------")
+
+#Esto es una constante... la idea de constantes no existe la idea de CONSTANTES, por lo que solo es una convencion... asi  que solo la traemos de la ideaa de otroa lenguajes, cog cog java, c++,etc.
+
+PI = 3.141516
+print("El valor de PI es", PI)
